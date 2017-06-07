0310ac605db6fb3169d7f42bbab02b1a4e1d1df9
Dovydas <dovydas.petrutis@gmail.com>
2017-06-07 14:30:35 +0300

    Added gitignore

---
+++ PoolManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PoolManager.Startup))]
+namespace PoolManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
