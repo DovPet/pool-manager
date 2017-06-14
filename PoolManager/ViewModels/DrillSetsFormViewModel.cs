ed61b5709f4154c3c3fe6c30a4f3e3afc19d9d7e
Rokas_Bundonis <rokas.bundonis.@ktu.edu>
2017-06-14 15:32:33 +0300

    DrillSets controller and views for admin.

---
+++ PoolManager/ViewModels/DrillSetsFormViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PoolManager.Models;
+
+namespace PoolManager.ViewModels
+{
+    public class DrillSetsFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Heading { get; set; }
+
+        [Required]
+        [AllowHtml]
+        public string Description { get; set; }
+        
+
+        public string Title
+        {
+            get
+            {
+                return Id != 0 ? "Edit DrillSet" : "New DrillSet";
+            }
+        }
+
+        public DrillSetsFormViewModel()
+        {
+            Id = 0;
+        }
+        public DrillSetsFormViewModel(DrillSet drillSet)
+        {
+            Id = drillSet.Id;
+            Heading = drillSet.Heading;
+            Description = drillSet.Description;
+
+        }
+    }
+}
